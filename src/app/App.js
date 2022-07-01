24401d7c3c463ffea9380296f69b0dfb64022567
LongWyrsch <long.nqw@gmail.com>
2022-07-01 13:46:15 +0200

    setting up project

---
+++ src/app/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import React from 'react';
+
+import { Inventory } from '../features/inventory/Inventory.js';
+import { CurrencyFilter } from '../features/currencyFilter/CurrencyFilter.js';
+// Import the Cart component here.
+
+// Render the Cart component below <Inventory />
+function App (props) {
+
+  const { state, dispatch } = props;
+
+  return (
+    <div>
+      <CurrencyFilter
+        currencyFilter={state.currencyFilter}
+        dispatch={dispatch}
+      />
+
+      <Inventory
+        inventory={state.inventory}
+        currencyFilter={state.currencyFilter}
+        dispatch={dispatch}
+      />
+
+    </div>
+  );
+};
+
+export default App;
